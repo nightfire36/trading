b3740f5561b509a2e4ddd174cdf0e54f50589b61
nightfire36 <57152643+nightfire36@users.noreply.github.com>
2020-02-11 21:37:01 +0100

    Delete TomcatInitializer.java

--- src/main/java/com/platform/trading/TomcatInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.platform.trading;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class TomcatInitializer extends SpringBootServletInitializer {
-
-}
